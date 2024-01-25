8f69c32289e3432d23d566ae78c6338473414af0
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-01-25 15:45:15 +0600

    error handle added

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import { ErrorRequestHandler } from 'express';
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Something went wrong!';
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    error: err,
+  });
+};
+
+export default globalErrorHandler;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,36 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log("Connected to MongoDB");
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+    console.log('Connected to MongoDB');
+
+    server = app.listen(config.port, () => {
+      console.log(`app is listening on port ${config.port}`);
     });
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
